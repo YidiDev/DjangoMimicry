8e6ee7b88c8df8dfc5e5f4533d910c5144aa3c15
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-08-11 18:43:10 -0400

    releasing a new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-mimicry',
-    version='1.6',
+    version='1.7',
     packages=find_packages(exclude=['djangoMimicryProject']),
     include_package_data=True,
     license='MIT License',
